70e11d967f25270f0de215cf1ec538d62c414822
Axon <2024.2xxx@gmail.com>
2026-06-02 04:57:17 +0000

    fix: update base path in Vite configuration to match project name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/x-template/',
+  base: '/x-template-special-001/',
   plugins: [
     react(),
     tailwindcss(),
